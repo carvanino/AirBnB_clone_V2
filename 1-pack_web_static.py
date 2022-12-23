cf82fd79037dad5ed6f62a2aee8142578a3935c8
Akinola <akinolatofunmi04@gmail.com>
2022-12-23 01:29:24 +0000

    Task 1 - Compress before sending;

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Generates a .tgz archive from the content of web_static folder
+using the do_pack function
+"""
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """ Genertes the archive files, stores them in a folder versions
+    """
+    try:
+        time = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+        arcfile = "versions/web_static_{}.tgz".format(time)
+
+        local('mkdir -p versions')
+        arclocal = local(
+                "tar -czvf {} web_static/".format(
+                    arcfile), capture=True)
+        return arclocal
+    except err:
+        return None
